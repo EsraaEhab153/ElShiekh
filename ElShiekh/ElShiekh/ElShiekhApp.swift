f8bf69871919faf2ea3c160ca2c91092b365a370
Esraa Ehab <esraaehab530@gmail.com>
2026-08-01 03:14:43 -0700

    initializing repository

---
+++ ElShiekh/ElShiekh/ElShiekhApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElShiekhApp.swift
+//  ElShiekh
+//
+//  Created by Esraa Ehab on 01/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct ElShiekhApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
